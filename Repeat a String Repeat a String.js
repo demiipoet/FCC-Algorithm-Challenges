c57861ef309d3b28033c7f5b014489e828b226a2
Justin <demiipoet@gmail.com>
2017-06-18 17:03:29 -0400

    Create Repeat a String Repeat a String.js

---
+++ Repeat a String Repeat a String.js
@@ -0,0 +1,17 @@
+
+function repeatStringNumTimes(str, num) {
+  
+  if (str.length >= 0) {
+    var numArr = [];
+    
+    for (var a = 0; a < num; a++) {
+      numArr.push(str);
+    }
+    return numArr.join('');
+  } else {
+    return 0;
+  }
+}
+
+repeatStringNumTimes("abc", 5); //Returns "abcabcabcabcabc"
+
