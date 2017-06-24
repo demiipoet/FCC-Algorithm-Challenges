aa15c43b825ce6378fc85a2f9da1485fc6a8b6b0
Justin <demiipoet@gmail.com>
2017-06-24 12:33:16 -0400

    Create Truncate a String.js

---
+++ Truncate a String.js
@@ -0,0 +1,16 @@
+
+function truncateString(str, num) {
+  
+  if (num > 3 && num >= str.length) {
+    return str;
+  
+  } else if (num > 3 && num < str.length) {
+    return str.slice(-str.length, num - 3) + "...";
+    
+  } else {
+    return str.slice(-str.length, num) + "...";
+    
+  }
+}
+
+truncateString("Pleasant-pheasant-plucker", 7); //Returns "Plea..."
