8d5ae4f3d9eb3abff8e01f8d7cea5055b218ef20
Justin <demiipoet@gmail.com>
2017-08-18 17:52:10 -0400

    Update Chunky Monkey.js

--- Chunky Monkey.js
+++ Chunky Monkey.js
@@ -1,3 +1,5 @@
+//Splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array
+
 function chunkArrayInGroups(arr, size) {
   var splitArr = [];
   
