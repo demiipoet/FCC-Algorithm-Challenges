dc03ae2d6931d48a5bf6fe2b6648ea7dc2ef5044
Justin <demiipoet@gmail.com>
2017-08-18 18:04:46 -0400

    Update Truncate a String.js

--- Truncate a String.js
+++ Truncate a String.js
@@ -1,3 +1,6 @@
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument)
+//Return the truncated string with a ... ending
+
 function truncateString(str, num) {
   
   if (num > 3 && num >= str.length) {
