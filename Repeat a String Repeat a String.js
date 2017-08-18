9b0fd57e4572d34b2d054198e2fa91296795fa29
Justin <demiipoet@gmail.com>
2017-08-18 18:07:02 -0400

    Update Repeat a String Repeat a String.js

--- Repeat a String Repeat a String.js
+++ Repeat a String Repeat a String.js
@@ -1,4 +1,5 @@
-//Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number
+//Repeat a given string (first argument) num times (second argument)
+//Return an empty string if num is not a positive number
 
 function repeatStringNumTimes(str, num) {
   
