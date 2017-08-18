38f14da230c70cc30b8199b5f5094ccab3244701
Justin <demiipoet@gmail.com>
2017-08-18 17:54:17 -0400

    Update Confirm the Ending.js

--- Confirm the Ending.js
+++ Confirm the Ending.js
@@ -1,3 +1,5 @@
+//Check if a string (first argument, str) ends with the given target string (second argument, target)
+
 function confirmEnding(str, target) {
   var endStr = str.substr(-target.length);
   
